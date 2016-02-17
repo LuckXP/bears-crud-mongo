a2c850b0c799cecef9a7c46ccbd888ef1f8a12f2
LuckXP <js.sindler@gmail.com>
2016-02-17 11:31:06 -0700

    wired the new bears.js file back to the server and the bear.js schema

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+var bearRouter = require('./routes/bears')
+
 router.use(function(req, res, next) {
 	console.log("something is happening")
 	next();
@@ -22,72 +24,7 @@ router.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!'});
 });
 
-router.route('/bears')
-	.post(function(req, res) {
-		var bear = new Bear();
-
-		bear.name = req.body.name;
-		bear.age = req.body.age;
-		bear.gender = req.body.gender;
-
-		bear.save(function(err, bear){
-			if(err){
-				console.log(err);
-			} else {
-				res.json(bear)
-			}
-		})
-	})
-	.get(function(req, res) {
-		Bear.find(function(err, bears){
-			if(err){
-				console.log(err);
-			} else {
-				res.json(bears);
-			}
-		})
-	});
-
-router.route('/bears/:bear_id')
-	.get(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if(err){
-				console.log(err);
-			} else {
-				res.json(bear);
-			}
-		})	
-	})
-	.put(function(req, res) {
-		Bear.findById(req.params.bear_id, function(err, bear) {
-			if(err){
-				console.log(err);
-			} else {
-				bear.name = req.body.name ? req.body.name : bear.name;
-				bear.age = req.body.age ? req.body.age : bear.age;
-				bear.gender = req.body.gender ? req.body.gender : bear.gender;
-				
-				bear.save(function(err){
-					if(err){
-						console.log(err);
-					} else {
-						res.json({title: "bear updated"});
-					}
-				})
-			}
-		})	
-	})
-	.delete(function(req, res) {
-		Bear.remove({_id :req.params.bear_id}, function(err, bear) {
-			if(err){
-				console.log(err);
-			} else {
-				res.json({title: "bear was deleted"});
-			}
-		})	
-	})
-
-app.use('/api', router);
+app.use('/api', bearRouter);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
